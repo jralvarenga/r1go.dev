2c4568408d4b7619cc278aa8f4160b1e3f090e8d
Rigo Alvarenga <jralvarenga161@gmail.com>
2026-02-12 12:14:58 -0600

    feat: SEO improvements

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -4,6 +4,7 @@ export const projects = [
     description: "Budget planner and expense tracker for web and mobile.",
     tech: ["Next.js", "Convex", "TypeScript", 'Polar'],
     href: "https://budio.r1go.dev",
+    iconUrl: "https://budio.r1go.dev/favicon.ico",
   },
   {
     title: "r1go.dev",
@@ -11,6 +12,7 @@ export const projects = [
       "Personal site to showcase projects and write about learnings.",
     tech: ["Astro", "TypeScript"],
     href: "https://r1go.dev",
+    iconUrl: "https://r1go.dev/favicon.ico",
     github: "https://github.com/r1go/r1go.dev",
   },
 ];
